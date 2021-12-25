f06e7b4648409951ac1ae21895c9299af88f5592
Andrew G <glazynovand@gmail.com>
2021-12-26 00:57:52 +0700

    add user mini profile ui

---
+++ src/components/mini-profile/MiniProfile.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import '../../fonts.css';
+import './MiniProfile.css';
+
+type MiniProfileInfo = {
+    firstName: string;
+    lastName: string;
+    nickname: string;
+    email: string;
+    location: string;
+    avatarLink: string;
+    isGuest: boolean;
+};
+
+export default function MiniProfile(info: MiniProfileInfo) {
+    return (
+        <div className={"root vertical-container"}>
+            <div
+                className={"vertical-container center-container"}
+                style={{width: "13vh"}}
+            >
+                <img className={"avatar"} src={info.avatarLink} alt={`${info.nickname} avatar`}/>
+            </div>
+
+            <div
+                className={"horizontal-container user-info-container center-container"}
+                style={{minWidth: "35vh"}}
+            >
+                <div>
+                    <div style={{fontSize: "2.5vh"}}>
+                        {info.lastName} {info.firstName} ({info.nickname})
+                    </div>
+                    <div style={{fontSize: "1.8vh"}}>
+                        <div>{info.email}</div>
+                        <div>{info.location}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
