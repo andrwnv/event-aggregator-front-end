317f41db793e08c5804adb978c5c46c2a18b7b9f
Andrew G <glazynovand@gmail.com>
2022-01-07 19:12:09 +0700

    add event card

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './fonts.css'
 import App from './App';
 
+import 'rsuite/dist/rsuite.min.css';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
