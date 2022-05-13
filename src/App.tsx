f702635564021331ab1160119532a8c51cd53af9
Andrew G <glazynovand@gmail.com>
2022-05-13 17:56:06 +0700

    add react-router v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 import RegistrationConfirmPage from './pages/RegistrationPage/RegistrationConfirmPage/RegistrationConfirmPage';
 import EventCard from './components/event-card/EventCard';
@@ -7,27 +9,35 @@ import EventCard from './components/event-card/EventCard';
 function App() {
     return (
         <div className="App">
-            {/*<RegistrationPage />*/}
+            <BrowserRouter>
+                <Routes>
+                    <Route path='registration'>
+                        <Route path='confirmed' element={<RegistrationConfirmPage />}/>
+                        <Route path='' element={<RegistrationPage />}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+
             {/*<RegistrationConfirmPage />*/}
-            <div style={{
-                display: 'flex',
-                width: '90%',
-                flexDirection: 'row',
-            }}>
-                {
-                    [1, 2, 3, 4].map(() => {
-                        return (
-                            <EventCard id={'123123-123-123'} title={'New Street Vision Art'}
-                                       shortText={'Съешь ещё этих мягких французских булок, да выпей же чаю! Съешь ещё этих мягких французских булок, да выпей же чаю!'}
-                                       location={'Россия, Томск'} pictureLinks={[
-                                'https://obzor.city/data/images/news_2021/06/street_vision/1_15_.jpg', 'https://www.riatomsk.ru/Upload/sub-9/49078_3.jpg',
-                                'https://obzor.city/data/images/news_2017/8/raznoe/dsc0146.jpg', 'https://www.riatomsk.ru/Upload/sub-9/49075_4.jpg',
-                                'https://proprikol.ru/wp-content/uploads/2020/05/kartinki-graffiti-45.jpg']}
-                            />
-                        );
-                    })
-                }
-            </div>
+            {/*<div style={{*/}
+            {/*    display: 'flex',*/}
+            {/*    width: '90%',*/}
+            {/*    flexDirection: 'row',*/}
+            {/*}}>*/}
+            {/*    {*/}
+            {/*        [1, 2, 3, 4].map(() => {*/}
+            {/*            return (*/}
+            {/*                <EventCard id={'123123-123-123'} title={'New Street Vision Art'}*/}
+            {/*                           shortText={'Съешь ещё этих мягких французских булок, да выпей же чаю! Съешь ещё этих мягких французских булок, да выпей же чаю!'}*/}
+            {/*                           location={'Россия, Томск'} pictureLinks={[*/}
+            {/*                    'http://127.0.0.1:9090/api/v1/load/6tJxjJAs4Os.jpg', 'https://www.riatomsk.ru/Upload/sub-9/49078_3.jpg',*/}
+            {/*                    'https://obzor.city/data/images/news_2017/8/raznoe/dsc0146.jpg', 'https://www.riatomsk.ru/Upload/sub-9/49075_4.jpg',*/}
+            {/*                    'https://proprikol.ru/wp-content/uploads/2020/05/kartinki-graffiti-45.jpg']}*/}
+            {/*                />*/}
+            {/*            );*/}
+            {/*        })*/}
+            {/*    }*/}
+            {/*</div>*/}
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './fonts.css'
 import App from './App';
 
 import 'rsuite/dist/rsuite.min.css';
+import './index.css';
+import './fonts.css'
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+    <App/>,
     document.getElementById('root')
 );
