1653ea9fa8c6cf86305c30fc594bd462ae8551af
Andrew G <glazynovand@gmail.com>
2022-05-20 16:11:38 +0700

    add 404 page

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import { Container, Content, FlexboxGrid, Header } from 'rsuite';
+import { useNavigate } from 'react-router-dom';
+
+import './NotFoundPage.css';
+
+
+export default function NotFoundPage() {
+    const navigate = useNavigate();
+
+    return (
+        <Container className={'root-container'}>
+            <Header style={{
+                position: 'fixed'
+            }}>
+                HI
+            </Header>
+            <Content>
+                <FlexboxGrid justify="center">
+                    <FlexboxGrid.Item colspan={20}>
+                        <Content style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            height: '100vh',
+                        }}>
+                            <div className={'form'}>
+                                <div className={'not-found'}>404</div>
+
+                                Данная страница не найдена
+
+                                <button className={"reg-button-base"}
+                                        style={{
+                                            backgroundColor: "#141C22",
+                                            color: "#D2D6D9",
+                                            marginLeft: 0,
+                                            marginTop: '25px'
+                                        }}>
+                                    <div
+                                        className={"second-font-size"}
+                                        style={{
+                                            paddingLeft: "2vw",
+                                            paddingRight: "2vw",
+                                        }}
+                                        onClick={() => {navigate('/', {replace: true})}}
+                                    >
+                                        На главную!
+                                    </div>
+                                </button>
+                            </div>
+                        </Content>
+                    </FlexboxGrid.Item>
+                </FlexboxGrid>
+            </Content>
+        </Container>
+    )
+}
