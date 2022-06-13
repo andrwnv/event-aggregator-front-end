3167c24bf9adf78c4ece833d8286c653fa9998e2
Andrew G <glazynovand@gmail.com>
2022-06-13 14:03:23 +0700

    add search bar to explore page

---
+++ src/components/search-bar/SearchBar.tsx
@@ -0,0 +1,61 @@
+import React, { useCallback, useRef, useState } from 'react'
+
+import { Input, InputGroup } from 'rsuite'
+import ClearIcon from '@rsuite/icons/Close'
+
+import debounce from 'lodash.debounce'
+
+import { ObjectType } from '../../api/const'
+
+import './SearchBar.css'
+
+type Item = {
+    type: ObjectType,
+    id: string
+}
+
+type SearchBarProps = {
+    foundItemsHandler: (items: Item[]) => void,
+}
+
+
+const SearchBar: React.FC<SearchBarProps> = ({foundItemsHandler}) => {
+
+    const [searchValue, setSearchValue] = useState<string>('')
+    const [, setInputValue] = useState<string>('')
+
+    const inputRef = useRef<HTMLInputElement | undefined>()
+
+    const onSearchClear = () => {
+        if (inputRef.current !== undefined)
+            inputRef.current!.value = ''
+        setSearchValue('')
+    }
+
+    const debounceSearch = useCallback(debounce((str: string) => {
+        setSearchValue(str)
+        console.log(str)
+    }, 1000), [])
+
+    const onChangeInput = (value: string) => {
+        setInputValue(value)
+        debounceSearch(value)
+    }
+
+    const SearchInputGroup = useCallback(() => (
+        <InputGroup size={'lg'} inside>
+            <Input placeholder={'Введите что хотите найти...'} onChange={onChangeInput} inputRef={inputRef}/>
+            <InputGroup.Button onClick={onSearchClear}>
+                <ClearIcon />
+            </InputGroup.Button>
+        </InputGroup>
+    ), [])
+
+    return (
+        <div className={'search-bar'}>
+            <SearchInputGroup />
+        </div>
+    )
+}
+
+export default SearchBar
--- src/pages/ExplorePage/ExplorePage.tsx
+++ src/pages/ExplorePage/ExplorePage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { Container, Sidebar, Sidenav, Nav, Navbar, FlexboxGrid } from 'rsuite'
 
 import ArrowRightLineIcon from '@rsuite/icons/ArrowRightLine'
@@ -9,35 +9,45 @@ import ClickableMapComponent from '../../components/clickable-map/ClickableMap'
 import { GeoPoint } from '../../misc/GeoPoint'
 import { ReactComponent as Logo } from '../../misc/icons/logo.svg'
 
+import LocationIcon from '@rsuite/icons/Location'
+import RandomIcon from '@rsuite/icons/Random'
+import TimeIcon from '@rsuite/icons/Time'
+import RelatedMapIcon from '@rsuite/icons/RelatedMap'
+import SearchIcon from '@rsuite/icons/Search'
+
 import './ExplorePage.css'
 import { useNavigate } from 'react-router-dom'
+import SearchBar from '../../components/search-bar/SearchBar'
 
 const NavToggle = ({ expand, onChange }: any) => {
     return (
         <Navbar appearance='subtle' className='nav-toggle'>
-            <Navbar.Body>
-                <Nav pullRight>
-                    <Nav.Item onClick={onChange} style={{ width: 56, textAlign: 'center' }}>
-                        {expand ? <ArrowLeftLineIcon /> : <ArrowRightLineIcon />}
-                    </Nav.Item>
-                </Nav>
-            </Navbar.Body>
+            <Nav pullRight>
+                <Nav.Item onClick={onChange} style={{ width: 56, textAlign: 'center' }}>
+                    {expand ? <ArrowLeftLineIcon /> : <ArrowRightLineIcon />}
+                </Nav.Item>
+            </Nav>
         </Navbar>
     )
 }
 
 export default function ExplorePage() {
     const [expand, setExpand] = useState(false)
-    const [active, setActive] = React.useState('1')
+    const [active, setActive] = React.useState('2')
     const [initialPosition, setInitialPosition] = useState<GeoPoint>({ lat: 55.755793, lon: 37.617134 })
     const navigate = useNavigate()
+
     // const {user, logout} = useAuth();
 
-    useEffect(() => {
+    const updateGeoLocation = () => {
         navigator.geolocation.getCurrentPosition(position => {
             const { latitude, longitude } = position.coords
             setInitialPosition({ lat: latitude, lon: longitude })
         })
+    }
+
+    useEffect(() => {
+        updateGeoLocation()
     }, [])
 
     const Map = useCallback(() => (
@@ -63,10 +73,11 @@ export default function ExplorePage() {
                 collapsible
             >
                 <Sidenav.Header>
-                    <div className={'explore-header'} onClick={() => navigate('/')}>
+                    <div className={'explore-header'} onClick={() => setExpand(!expand)}>
                         <FlexboxGrid justify={'center'} align={'middle'}>
                             <Logo fill={'#fff'} style={{ width: '2em' }} />
-                            <span style={expand ? { display: 'flex', marginLeft: 12 } : {display: 'none'} } className={'explore-header-title'}> Take place</span>
+                            <span style={expand ? { display: 'flex', marginLeft: 12 } : { display: 'none' }}
+                                  className={'explore-header-title'}> Take place</span>
                         </FlexboxGrid>
                     </div>
                 </Sidenav.Header>
@@ -75,11 +86,32 @@ export default function ExplorePage() {
                         <Nav activeKey={active} onSelect={(eventKey) => {
                             setActive(eventKey)
                         }}>
-                            <Nav.Item eventKey='1' icon={<Dashboard />}>
-                                Dashboard
+                            {
+                                expand
+                                    ? <SearchBar foundItemsHandler={(items) => {
+                                    }} />
+                                    : <Nav.Item eventKey='0' icon={<SearchIcon />} onClick={() => {
+                                        setExpand(true)
+                                    }}>Поиск</Nav.Item>
+                            }
+
+                            <Nav.Item eventKey='1' icon={<Dashboard />} onClick={() => {
+                                setActive('2')
+                                navigate('/')
+                            }}>
+                                На главную
                             </Nav.Item>
-                            <Nav.Item eventKey='2'>
-                                User Group
+                            <Nav.Item eventKey='2' icon={<LocationIcon />} onClick={updateGeoLocation}>
+                                Рядом со мной
+                            </Nav.Item>
+                            <Nav.Item eventKey='3' icon={<TimeIcon />}>
+                                Только события
+                            </Nav.Item>
+                            <Nav.Item eventKey='4' icon={<RelatedMapIcon />}>
+                                Только места
+                            </Nav.Item>
+                            <Nav.Item eventKey='5' icon={<RandomIcon />}>
+                                Вперемешку
                             </Nav.Item>
                         </Nav>
                     </Sidenav.Body>
