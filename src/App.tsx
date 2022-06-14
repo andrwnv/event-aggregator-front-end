261f406f214e36c1b2708c07951fb00e99bfd63d
Andrew G <glazynovand@gmail.com>
2022-06-14 19:56:51 +0700

    add history page, histories list page. Histories loaded from server

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import RegistrationConfirmPage from './pages/RegistrationPage/RegistrationConfirmPage/RegistrationConfirmPage'
 import AuthenticatedRoute from './components/custom-roures/AuthenticatedRoute'
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage'
+import HistoryListPage from './pages/HistoryListPage/HistoryListPage'
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+import HistoryPage from './pages/HistoryPage/HistoryPage'
 import ExplorePage from './pages/ExplorePage/ExplorePage'
 import ProfilePage from './pages/ProfilePage/ProfilePage'
+import ObjectPage from './pages/ObjectPage/ObjectPage'
 import SignInPage from './pages/SignInPage/SignInPage'
 import MainPage from './pages/MainPage/MainPage'
 
 import { AuthProvider } from './hooks/useAuth'
+import { ObjectType } from './api/const'
 
 import 'rsuite/styles/index.less'
 import './App.css'
-import ObjectPage from './pages/ObjectPage/ObjectPage'
-import { ObjectType } from './api/const'
 
-function App() {
+
+const App: React.FC<any> = () => {
     return (
         <div className='App'>
             <BrowserRouter>
@@ -34,7 +37,12 @@ function App() {
                         />
                         <Route path='/event/:id' element={<ObjectPage type={ObjectType.EVENT} />}/>
                         <Route path='/place/:id' element={<ObjectPage type={ObjectType.PLACE} />}/>
+
+                        <Route path='/histories/:id' element={<HistoryPage />} />
+                        <Route path='/histories' element={<HistoryListPage />} />
+
                         <Route path='explore' element={<ExplorePage />} />
+
                         <Route path='sign_in' element={<SignInPage />} />
                         <Route path='sign_up'>
                             <Route path='confirmed' element={<RegistrationConfirmPage />} />
